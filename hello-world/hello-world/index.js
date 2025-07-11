0501c1aad8773fbacc7d60acb3dea47ef0e779aa
Hamsini N <hamsini.nj@outlook.com>
2025-07-11 16:54:08 +0530

    Add files via upload

---
+++ hello-world/hello-world/index.js
@@ -0,0 +1,15 @@
+hello = "Hello GitHub!";
+const returnHello = () => {
+  console.log(hello);
+};
+returnHello();
+
+/*const http = require("http");
+const fs = require("fs");
+const server = http.createServer((req, res) => {
+  fs.readFile("sample.txt", (err, data) => {
+    res.end(data);
+  });
+});
+server.listen(3000);
+*/
